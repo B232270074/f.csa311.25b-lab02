8465c571552b395a4fc00df35579b211bf88f3d5
B232270074 <baljirusukhbayr0711@gmail.com>
2025-02-21 18:31:58 +0800

    5-7

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,13 +1,20 @@
 package edu.cmu.cs.cs214.lab02;
 
-import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        Shape rectangle = new Rectangle(2, 3);
+        Shape circle = new Circle(5);
+        Shape square = new Square(4);
 
-        Renderer renderer = new Renderer(rectangle);
+        Renderer rectangleRenderer = new Renderer(rectangle);
+        Renderer circleRenderer = new Renderer(circle);
+        Renderer squareRenderer = new Renderer(square);
 
-        renderer.draw();
+        rectangleRenderer.draw();
+        circleRenderer.draw();
+        squareRenderer.draw();
+        System.out.println(rectangle.getArea());
     }
 }
--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
@@ -1,15 +1,34 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Rectangle implements Shape {
-    public double height;
-    public double width;
+    private double height;
+    private double width;
     
     public Rectangle(double height, double width){
         this.height = height;
         this.width = width;
     }
-
+    public double getPerimeter() {
+        return 2 * (height + width);
+    }
+    public void setHeight(double height) {
+        this.height = height;
+    }
+    public void setWidth(double width) {
+        this.width = width;
+    }
+    public double getHeight() {
+        return height;
+    }
+    public double getWidth() {
+        return width;
+    }
+    @Override
     public double getArea() {
         return height * width;
     }
+    @Override public String getShapeName(){
+        return "Rectangle";
+    }
+    
 }
